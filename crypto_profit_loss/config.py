c2c9cf867638a4d4573420ebd94479061f612b10
rafmagns-skepa-dreag <richard.sonofhans@gmail.com>
2018-05-11 23:18:44 -0500

    lol committed my api keys. you can use them if you really want to I guess

--- crypto_profit_loss/config.py
+++ crypto_profit_loss/config.py
@@ -1,3 +1,4 @@
+# These are my keys. They don't have trade access. So use them if you want?
 API_KEY = 'EEbQX5GoQCsedhwBpPu9hOCVA7z8tCwDO9CP7izxtsnGWc3iuT4heuNP9cIpAQYr'
 API_SECRET = 'S0dGTxBwYwOo0zD61MMfXMfqKB5OFMrdjahOujBVX6blaQndjC1D1nefd2vvTkoU'
 USER_ID = 1
